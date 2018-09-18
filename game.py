523922de1f7c8a6a4d38a21ff69108ef84e29159
Brian Lobo <brianlobo.code@gmail.com>
2018-09-17 21:45:17 -0400

    Created pygame window that responds to user input

--- game.py
+++ game.py
@@ -1,5 +1,22 @@
-def main():
-    pass
+import sys
+import pygame
+
+def run_game():
+    # Init game and create a screen object
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Galactic War")
+
+    # Start the main loop for the game
+    while True:
+        # Watch for keyboard and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        # Make the most recently drawn screen visable
+        pygame.display.flip()
+
+run_game()
 
 if __name__ == '__main__':
-    main()
+    run_game()
