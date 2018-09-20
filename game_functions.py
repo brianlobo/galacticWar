79bfab5049cd56d041163a25811499653e245b55
Brian Lobo <brianlobo.code@gmail.com>
2018-09-20 10:44:16 -0400

    Added enemy ship and Q now quits the game

--- game_functions.py
+++ game_functions.py
@@ -21,6 +21,8 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_SPACE:
         # Creat new bullet and add it to bullets group
         fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 def check_keyup_events(event, ship):
     # Responds to key releases
